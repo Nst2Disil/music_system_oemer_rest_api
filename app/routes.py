e5c9fbf1fdadc6ca6abb9d192b0fab3cb9e2d4b4
Anastasia Tuzova <nsttuzova@yandex.ru>
2024-06-09 01:37:37 +0300

    Add oemer status enpoind and delete previous results

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-from flask import request, send_file
+from flask import request, send_file, jsonify
 
 from app import app
 
@@ -22,6 +22,13 @@ def recognize():
             try:
                 img_path = os.path.join(INPUT_PATH, IMG_NAME)
                 file.save(img_path)
+
+                # удаление предыдущих результатов распознавания
+                file_list = os.listdir(OUTPUT_PATH)
+                for file_name in file_list:
+                    file_path = os.path.join(OUTPUT_PATH, file_name)
+                    os.remove(file_path)
+
                 oemer_process = run_oemer(img_path)
                 
                 musicxml_path = os.path.join(OUTPUT_PATH, MUSICXML_NAME)
@@ -35,6 +42,18 @@ def recognize():
             return {"error": "No file provided"}, 400  # Bad Request
     else:
         return {"info": "Sorry, the request cannot be processed. Try later."}, 202  # Accepted 
+
+
+@app.route('/v1/oemer/status', methods=['GET'])
+def check_status():
+    try:
+        global oemer_process
+        if oemer_process is None or oemer_process.poll() is not None:
+            return jsonify({"free": True})
+        else:
+            return jsonify({"free": False})
+    except Exception as e:
+        return {"error": str(e)}, 500  # Internal Server Error
 
 
 def run_oemer(img_path):
